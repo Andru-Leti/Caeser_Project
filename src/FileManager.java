6def4fa115a5ae21e2c30186b8b031cb0a7b81d2
Andrew <andrey210608@gmail.com>
2026-03-28 13:19:54 +0300

    1.Add new class FileManager 2.Add new method in FileManager: fileReader, fileWriter

---
+++ src/FileManager.java
@@ -0,0 +1,38 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.InvalidPathException;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class FileManager {
+
+    public List<String> fileReader(String fileName) throws IOException {
+        List<String> result = new ArrayList<>();
+
+        try(BufferedReader reader = Files.newBufferedReader(Path.of(fileName), StandardCharsets.UTF_8)) {
+            String line;
+            while((line = reader.readLine()) != null){
+                result.add(line);
+            }
+        } catch(IOException | InvalidPathException e){
+            throw new IOException();
+        }
+        return result;
+    }
+
+    public void fileWriter(String fileName, List<String> data){
+        try(BufferedWriter bufferedWriter = Files.newBufferedWriter(Path.of(fileName), StandardCharsets.UTF_8)){
+            for(String string: data){
+                bufferedWriter.write(string);
+                bufferedWriter.newLine();
+            }
+        } catch (IOException | InvalidPathException e) {
+            throw new IOException();
+        }
+    }
+}
